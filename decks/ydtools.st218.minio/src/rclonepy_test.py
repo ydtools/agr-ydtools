5ef9a4bcad4fcdeae017560c6496a4d4a02fef5d
Your Name <you@example.com>
2021-05-31 06:25:09 +0300

    working tests for rcloe and s3path

--- decks/ydtools.st218.minio/src/rclonepy_test.py
+++ decks/ydtools.st218.minio/src/rclonepy_test.py
@@ -13,7 +13,7 @@ def test_rclone_tree():
     print(the_tree)
 
 
-def test_rclone_tree():
+def test_rclone_tree2():
     import rclonepy
     from pathlib import Path
     
@@ -22,3 +22,17 @@ def test_rclone_tree():
 
     the_data = rclonepy.get_data('minio:test-b/hw.text.txt')
     assert 'Hello' in the_data
+
+def test_rclone_tree3():
+    import urllib.request as urlreq
+    from io import StringIO as strio
+
+    class PyProtoHandler(urlreq.BaseHandler):
+        def python_open(self, req):
+            fullUrl = req.get_full_url()
+            return strio(fullUrl)
+
+    opener = urlreq.build_opener(PyProtoHandler())
+    urlreq.install_opener(opener)
+    print(urlreq.urlopen("python://something/random/file.txt").read())
+    print(len(urlreq.urlopen("http://example.com").read()))
