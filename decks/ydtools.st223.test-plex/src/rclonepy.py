cb1baac51a23c94d7a78795a5159d77bd0636c55
Your Name <you@example.com>
2021-05-31 13:20:20 +0300

    all tests are passing

--- decks/ydtools.st223.test-plex/src/rclonepy.py
+++ decks/ydtools.st223.test-plex/src/rclonepy.py
@@ -59,17 +59,21 @@ def copyto(source:Path, dest:Path):
 def get_data(source):
     import io
     import time
-    
+    import os
     shell = spur.LocalShell()
 
     output_file = io.StringIO()
+    update_env = {
+        'RCLONE_CONFIG': os.environ.get('RCLONE_CONFIG_FILE__HOST_EXT'),
+    }    
     process = shell.spawn(
         ["rclone", "cat", source],
         stdout=output_file,
         encoding="utf-8",
+        update_env=update_env
     )
-    time.sleep(1)
 
+    process.wait_for_result()
     data = output_file.getvalue()
     # print(data)
     return data
