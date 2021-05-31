5da776dd35204fd6c1f98be90b66776889c79640
Your Name <you@example.com>
2021-05-31 13:42:56 +0300

    make it work

--- decks/ydtools.st218.minio/src/rclonepy_old1_test.py
+++ decks/ydtools.st218.minio/src/rclonepy_old1_test.py
@@ -13,16 +13,6 @@ def test_rclone_tree():
     print(the_tree)
 
 
-def test_rclone_tree2():
-    import rclonepy
-    from pathlib import Path
-    
-    me_parent_p = Path(__file__).parent
-    name = Path(__file__).name
-
-    the_data = rclonepy.get_data('minio:test-b/hw.text.txt')
-    assert 'Hello' in the_data
-
 def test_rclone_tree3():
     import urllib.request as urlreq
     from io import StringIO as strio
